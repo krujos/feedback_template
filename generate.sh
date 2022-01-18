6ee4fe2200e04af490dd9f35bf04d8d109430852
Josh Kruck <joshua.kruck@gmail.com>
2022-01-17 20:25:40 -0600

    understand first and last names, language tweak

--- generate.sh
+++ generate.sh
@@ -1,16 +1,21 @@
 #/bin/sh 
 
-PERSON=${1}
+FIRST_NAME=${1}
+LAST_NAME=${2}
 
-
+if [ -z "${FIRST_NAME}" -o -z "${LAST_NAME}" ]; then
+    echo "USAGE: generate.sh FIRST_NAME LAST_NAME\n"
+    exit 1;
+fi
+     
 cat << EOF
-Thank you for giving feedback on ${PERSON}, while perhaps uncomfortable this is important for the development of our organization and ${PERSON}. Please take a few minutes to answer the questions below. All feedback will be kept confidential and synthesized prior to being shared with ${PERSON}
+Thank you for giving feedback on ${FIRST_NAME} ${LAST_NAME}, while perhaps uncomfortable your feedback is important for the development of our organization and ${FIRST_NAME}. Please take a few minutes to answer the questions below. All feedback will be kept confidential and synthesized prior to being shared with ${FIRST_NAME}
 
-What is ${PERSON}'s greatest strength, how should they use it next year? 
+What is ${FIRST_NAME}'s greatest strength, how should they use it next year? 
 
-What is ${PERSON}’s greatest weakness, how could they improve next year?
+What is ${FIRST_NAME}’s greatest weakness, how could they improve next year?
 
-You have 30 seconds to describe ${PERSON} to a stranger, what would you say? 
+You have 30 seconds to describe ${FIRST_NAME} to a stranger, what would you say? 
 
 EOF
 
