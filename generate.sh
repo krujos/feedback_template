39f4c7e5a46446281aabe2ca46c2cf2a383d9539
Josh Kruck <joshua.kruck@gmail.com>
2022-01-17 20:13:44 -0600

    Add boilerplate

---
+++ generate.sh
@@ -0,0 +1,12 @@
+#/bin/sh 
+
+`cat << EOF | 
+Thank you for giving feedback on ${RECEIVER}, while perhaps uncomfortable this is important for the development of our organization and {PERSON}. Please take a few minutes to answer the questions below. All feedback will be kept confidential and synthesized prior to being shared with ${RECEIVER}
+
+What is ${PERSON}’s greatest strength, how should they use it next year? 
+
+What is ${PERSON}’s greatest weakness, how could they improve next year?
+
+You have 30 seconds to describe ${PERSON} to a stranger, what would you say? 
+
+EOF
